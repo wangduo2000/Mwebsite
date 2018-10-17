223d64c188b9922fc4a896304d615c99f1d0c441
Agnes <1309146734@qq.com>
2018-10-17 23:08:44 +0800

    liujia

--- src/scripts/controllers/sendexpress.js
+++ src/scripts/controllers/sendexpress.js
@@ -1,7 +1,24 @@
 import sendexpressTpl from '../views/sendexpress.html'
+import sendListTpl from '../views/send-list.html'
+import sendModel from '../models/send'
 
-const render = () => {
-  $('main').html(sendexpressTpl)
+const render = async () => {
+  $('main').html(sendexpressTpl);
+  let list =(await sendModel.list()).data;
+  // let imgs =(await sendModel.list()).data.comlist;
+  console.log(list);
+  await renderList(list);
+  $(document).ready(loading());
+}
+
+const renderList = async (list) => {
+  let template = Handlebars.compile(sendListTpl)
+  let html = template({ list })
+  $('#ul').html(html)
+}
+
+let loading = ()=>{
+  $('#loading').css("display","none");
 }
 
 export default {
